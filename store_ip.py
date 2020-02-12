323d1dff97917fe2c238305c0a3b86e8252e0bdb
HelgeCPH <ropf@itu.dk>
2020-02-12 13:17:41 +0100

    Updated requirements

--- store_ip.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-for line in sys.stdin:
-    if line.startswith("==> dbserver: Assigned IP address:"):
-        with open("db_ip.txt", "w") as fp:
-            fp.write(line.split()[-1])
-    print(line, end="")
